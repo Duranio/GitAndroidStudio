26a290feefe8a2e53bd51b84a801151b3da18f80
Duranio <mvucap@tin.it>
2021-03-06 22:14:42 +0100

    Second message

--- app/src/main/java/com/duranio/gitandroidstudio/MainActivity.java
+++ app/src/main/java/com/duranio/gitandroidstudio/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("GIT_LOG", "onCreate: ");
+        Log.d("GIT_LOG", "onCreate: second message");
+
     }
 }
